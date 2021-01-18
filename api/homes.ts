8e7d63b21ea6106249f8cc18e291e2a40bc4d019
Carter Grimmeisen <cpg0009@uah.edu>
2021-01-18 11:53:22 -0600

    Initial usage of prisma and postgres rather than mongodb

--- api/homes.ts
+++ api/homes.ts
@@ -1,9 +1,40 @@
 import { Router } from 'express'
 
+// import HOME_DATA from './home_data.json'
+
 const router = Router()
 
-router.route('/homes').get((_, res) => {
-  res.json({ data: 'data' })
+router.get('/homes', async (req, res) => {
+  // res.json(await Home.find())
+  // const user = await User.findOne()
+
+  console.log('WAITING FOR SOMETHING')
+  const user = await req.prisma.user.findFirst()
+
+  console.log('DONE WAITING FOR SOMETHING')
+  res.json(user)
+})
+
+router.post('/homes', async (req, res) => {
+  // await Home.remove({})
+  // Home.find({  })
+  // const user = await User.create({
+  //   username: 'Hello',
+  //   email: 'hello@world.com',
+  //   type: 'admin',
+  //   hash: '123456789',
+  // })
+  console.log('WAITING FOR SOMETHING')
+  const user = await req.prisma.user.create({
+    data: {
+      name: 'Hello World',
+      email: 'hello@world.com',
+      type: 'ADMIN',
+      hash: '123456789',
+    },
+  })
+
+  res.json(user)
 })
 
 export default router
--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,17 @@
 import bodyParser from 'body-parser'
+// import mongoose from 'mongoose'
+import { PrismaClient } from '@prisma/client'
 import App from 'express'
 
 import Homes from './homes'
 
+const prisma = new PrismaClient()
+
 const app = App()
+
+app.use((req) => {
+  req.prisma = prisma
+})
 
 app.use(bodyParser.json())
 app.use(Homes)
