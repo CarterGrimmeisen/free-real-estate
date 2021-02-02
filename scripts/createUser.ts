cbb51349ccf1bcade31e71458de594ba3de99141
Carter Grimmeisen <cpg0009@uah.edu>
2021-02-02 10:59:58 -0600

    A bunch of changes that fix scripts and update some dependencies

--- scripts/createUser.ts
+++ scripts/createUser.ts
@@ -2,85 +2,87 @@
 
 /* eslint-disable no-console */
 import enquirer from 'enquirer'
-import Prisma from '@prisma/client'
+import { PrismaClient, UserType } from '@prisma/client'
 import bcrypt from 'bcryptjs'
 
 type Values = {
   name: string
   email: string
   password: string
-  type: Prisma.UserType
+  type: UserType
 }
 
-const prisma = new Prisma.PrismaClient()
+async function main() {
+  const prisma = new PrismaClient()
 
-const { name, email, password, type } = await enquirer.prompt<Values>([
-  {
-    type: 'input',
-    name: 'name',
-    message: "👤 What is your new user's name?",
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: "📧 What is your new user's email address?",
-  },
-  {
-    type: 'password',
-    name: 'password',
-    message: "🔒 What is your new user's password?",
-  },
-  {
-    type: 'select',
-    name: 'type',
-    message: "🔑 What is your new user's type?",
-    initial: 0,
-    choices: [
-      { name: 'USER', message: 'User' },
-      { name: 'AGENT', message: 'Agent' },
-      { name: 'ADMIN', message: 'Admin' },
-    ],
-  },
-])
+  const { name, email, password, type } = await enquirer.prompt<Values>([
+    {
+      type: 'input',
+      name: 'name',
+      message: "👤 What is your new user's name?",
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "📧 What is your new user's email address?",
+    },
+    {
+      type: 'password',
+      name: 'password',
+      message: "🔒 What is your new user's password?",
+    },
+    {
+      type: 'select',
+      name: 'type',
+      message: "🔑 What is your new user's type?",
+      initial: 0,
+      choices: [
+        { name: 'USER', message: 'User' },
+        { name: 'AGENT', message: 'Agent' },
+        { name: 'ADMIN', message: 'Admin' },
+      ],
+    },
+  ])
 
-const exists = await prisma.user.count({
-  where: {
-    email,
-  },
-})
-
-if (exists) {
-  console.error(
-    `❗ User with email '${email}' already exists, please choose a different one and try again.`
-  )
-  process.exit(1)
-}
-
-let user
-try {
-  user = await prisma.user.create({
-    data: {
-      name,
+  const exists = await prisma.user.count({
+    where: {
       email,
-      password: await bcrypt.hash(password, 10),
-      type,
     },
   })
-} catch (e) {
-  console.error(e)
-  console.error(
-    `❗ User could not be created, check the error message present above and try again`
-  )
+
+  if (exists) {
+    console.error(
+      `❗ User with email '${email}' already exists, please choose a different one and try again.`
+    )
+    process.exit(1)
+  }
+
+  let user
+  try {
+    user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: await bcrypt.hash(password, 10),
+        type,
+      },
+    })
+  } catch (e) {
+    console.error(e)
+    console.error(
+      `❗ User could not be created, check the error message present above and try again`
+    )
+  }
+
+  if (user) {
+    console.log(
+      `✔️ User successfully created with email '${email}' and password '${password}'`
+    )
+  } else {
+    console.error(
+      `❗ User could not be created, check the error message present above and try again`
+    )
+  }
 }
 
-if (user) {
-  console.log(
-    `✔️ User successfully created with email '${email}' and password '${password}'`
-  )
-} else {
-  console.error(
-    `❗ User could not be created, check the error message present above and try again`
-  )
-}
-
-process.exit(0)
+main().then(() => process.exit(0))
