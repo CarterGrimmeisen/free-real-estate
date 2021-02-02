7b8fe6b85766b134f2c3ad23de8fb988337b8690
Lane <iamaperson620@gmail.com>
2021-02-02 07:02:59 +0000

    added logic for reading and modifying the database

--- scripts/sendEmail.ts
+++ scripts/sendEmail.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 import Prisma from '@prisma/client'
 import NodeMailer from 'nodemailer'
 import Mail from 'nodemailer/lib/mailer'
@@ -13,22 +15,45 @@ const transporter = NodeMailer.createTransport({
   },
 })
 
-const agents = await prisma.agent.findMany()
+const agents = await prisma.agent.findMany({
+  include: {
+    homes: true,
+  },
+})
+
+console.log(agents)
 
 for (const agent of agents) {
+  let html: string = ''
+  let totalHits: number = 0
+
   for (const house of agent.homes) {
-    console.log(house)
+    html += `<h1>${house.street} in ${house.city}, ${house.state} ${house.zipcode}</h1><br />Got <strong>${house.dailyHits}</strong> hits!<br />`
+    totalHits += house.dailyHits
+
+    await prisma.home.update({
+      where: {
+        mlsn: house.mlsn,
+      },
+      data: {
+        dailyHits: 0,
+      },
+    })
   }
 
   const mailOptions: Mail.Options = {
     from: process.env.EMAIL_USRN,
     to: agent.email,
-    subject: 'You got 4 hits today!',
-    html: 'can i use <em>html</em> perhaps?',
+    subject: `You got ${totalHits} hits today!`,
+    html,
   }
+
+  console.log(JSON.stringify(mailOptions))
 
   transporter.sendMail(mailOptions, (err, _) => {
     if (err) console.log(err)
     else console.log(`Email sent to ${agent.email}`)
   })
 }
+
+process.exit(0)
