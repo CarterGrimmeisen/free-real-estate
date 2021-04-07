9ae3d41f9059bad9cf5b52f1abd591ba664a2b60
Carter Grimmeisen <cpg0009@uah.edu>
2021-04-06 23:17:04 -0500

    Fixed linting errors

--- components/detail-listing/EditListing/ImageEditor.vue
+++ components/detail-listing/EditListing/ImageEditor.vue
@@ -35,22 +35,23 @@ import { defineComponent } from '@vue/composition-api'
 export default defineComponent({
   name: 'HouseImages',
   components: {},
-  setup() {},
-  data: () => ({
-    items: [
-      {
-        src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-      },
-      {
-        src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-      },
-      {
-        src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-      },
-      {
-        src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-      },
-    ],
-  }),
+  setup() {
+    return {
+      items: [
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+        },
+      ],
+    }
+  },
 })
 </script>
