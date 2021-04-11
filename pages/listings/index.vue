f99eb34ed5c434e3538d4b513b1a775f6c6d63e3
Carter Grimmeisen <cpg0009@uah.edu>
2021-04-11 13:12:24 -0500

    Got infinite scroll working and fixed more details button

--- pages/listings/index.vue
+++ pages/listings/index.vue
@@ -3,34 +3,72 @@
     <div>
       <FilterBar />
       <div class="container">
-        <div>
-          <v-row v-if="homesReady && homes" no-gutters>
-            <v-col v-for="home in homes" :key="home.mlsn" cols="4">
-              <ListingPreview :home="home" />
-            </v-col>
-          </v-row>
-        </div>
+        <v-row v-if="loadedHomes.length" v-scroll="onScroll" no-gutters>
+          <v-col v-for="home in loadedHomes" :key="home.mlsn" cols="4">
+            <ListingPreview :home="home" />
+          </v-col>
+        </v-row>
+        <v-row v-if="loadingMore">
+          <v-progress-linear indeterminate></v-progress-linear>
+        </v-row>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, useContext } from '@nuxtjs/composition-api'
-import { useHomes, useData } from '@/hooks/api'
+import {
+  defineComponent,
+  ref,
+  watch,
+  useContext,
+} from '@nuxtjs/composition-api'
+import { useHomes } from '@/hooks/api'
+import { HomeWithImage } from '~/api/api'
 
 export default defineComponent({
   setup() {
     const { $auth } = useContext()
     const { getHomes } = useHomes()
 
-    const [homes, homesReady] = useData(getHomes)
+    const loadingMore = ref(false)
+    const doneLoading = ref(false)
+    const loadedPages = ref(0)
+    const loadedHomes = ref<HomeWithImage[]>([])
+    const distanceToBottom = ref(0)
+    const onScroll = () => {
+      distanceToBottom.value = Math.max(
+        document.body.offsetHeight - (window.pageYOffset + window.innerHeight),
+        0
+      )
+    }
+
+    watch(
+      distanceToBottom,
+      async () => {
+        if (
+          distanceToBottom.value < 550 &&
+          !loadingMore.value &&
+          !doneLoading.value
+        ) {
+          loadingMore.value = true
+          const newHomes = await getHomes(
+            {},
+            { take: 12, skip: loadedPages.value++ * 12 }
+          )
+          if (!newHomes.length) doneLoading.value = true
+          loadedHomes.value = [...loadedHomes.value, ...newHomes]
+          loadingMore.value = false
+        }
+      },
+      { immediate: true }
+    )
 
     return {
-      data: { working: true },
       $auth,
-      homes,
-      homesReady,
+      onScroll,
+      loadedHomes,
+      loadingMore,
     }
   },
 })
