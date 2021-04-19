fd4ec01dc072288a888d5597493630355226a57e
emmy <ev0007@uah.edu>
2021-04-19 17:57:05 -0500

    Document generation progress

--- pages/listings/_mlsn/documents.vue
+++ pages/listings/_mlsn/documents.vue
@@ -7,8 +7,8 @@
 
       <!-- start of second grid -->
 
-      <v-container class="grey lighten-5">
-        <v-row no-gutters>
+      <v-container class="grey lighten-5" fluid>
+        <v-row>
           <v-col cols="6" md="4">
             <v-card height="400" width="256" class="mx-auto">
               <v-navigation-drawer permanent>
@@ -21,20 +21,39 @@
                 </v-list-item>
 
                 <v-divider></v-divider>
-                <v-btn class="ma-2" color="primary" dark width="230">
+                <v-btn
+                  class="ma-2"
+                  color="primary"
+                  dark
+                  width="230"
+                  @click.native="selectPDF = 'closing'"
+                >
                   Closing Disclosure
                   <v-icon dark right> mdi-garbage </v-icon>
                 </v-btn>
                 <v-divider></v-divider>
-                <v-btn class="ma-2" color="primary" dark width="230">
+                <v-btn
+                  class="ma-2"
+                  color="primary"
+                  dark
+                  width="230"
+                  @click.native="selectPDF = 'purchase'"
+                >
                   Purchase Agreement
                   <v-icon dark right> mdi-garbage </v-icon>
                 </v-btn>
                 <v-divider></v-divider>
-                <v-btn class="ma-2" color="primary" dark width="230">
+                <v-btn
+                  class="ma-2"
+                  color="primary"
+                  dark
+                  width="230"
+                  @click.native="selectPDF = 'repair'"
+                >
                   Repair Request
                   <v-icon dark right> mdi-garbage </v-icon>
                 </v-btn>
+
                 <v-divider></v-divider>
                 <v-list dense nav>
                   <!-- <v-list-item v-for="item in items" :key="item.title" link>
@@ -50,30 +69,67 @@
               </v-navigation-drawer>
             </v-card>
           </v-col>
-          <v-col cols="12" sm="6" md="8">
-            <v-card class="pa-2" width="1150" height="700" outlined title>
-              <v-img></v-img>
+          <v-col>
+            <v-row>
+              <v-col cols="12" sm="6" md="8">
+                <v-card class="pa-2" width="1150" outlined title>
+                  <v-card-title>Document Preview</v-card-title>
+                  <v-divider class="mx-4"></v-divider>
 
-              <v-card-title>Document Preview</v-card-title>
-              <v-divider class="mx-4"></v-divider>
-
-              <v-card-text>
-                <div class="text-left" size="14">
-                  <v-card-text>
-                    This is where I want the document preview
-                  </v-card-text>
-                </div>
-              </v-card-text>
-
-              <v-divider class="mx-4"></v-divider>
-
-              <v-card-actions>
-                <v-btn class="ma-2" color="primary" dark>
-                  Download
-                  <v-icon dark right> </v-icon>
-                </v-btn>
-              </v-card-actions>
-            </v-card>
+                  <v-row>
+                    <v-col>
+                      <v-card-text>
+                        <h1 v-if="selectPDF == 'closing'">
+                          Closing Disclosure
+                        </h1>
+                        <h1 v-else-if="selectPDF == 'purchase'">
+                          Purchase Agreement
+                        </h1>
+                        <h1 v-else-if="selectPDF == 'repair'">
+                          Request Repair
+                        </h1>
+                        <h1 v-else>No Form Selected</h1>
+                      </v-card-text>
+                    </v-col>
+                    <v-col>
+                      <v-card-actions>
+                        <v-btn
+                          v-if="selectPDF != 'none'"
+                          class="ma-2"
+                          color="primary"
+                          dark
+                        >
+                          Download
+                          <v-icon dark right> </v-icon>
+                        </v-btn>
+                      </v-card-actions>
+                    </v-col>
+                  </v-row>
+                </v-card>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12" sm="6" md="8">
+                <iframe
+                  v-if="selectPDF == 'closing'"
+                  height="800"
+                  width="100%"
+                  src="/ClosingDisclosure.pdf#toolbar=0&navpanes=0"
+                />
+                <iframe
+                  v-else-if="selectPDF == 'purchase'"
+                  height="800"
+                  width="100%"
+                  src="/PurchaseAgreement.pdf#toolbar=0&navpanes=0"
+                />
+                <iframe
+                  v-else-if="selectPDF == 'repair'"
+                  height="800"
+                  width="100%"
+                  src="/RepairRequest.pdf#toolbar=0&navpanes=0"
+                />
+              </v-col>
+            </v-row>
           </v-col>
         </v-row>
       </v-container>
@@ -83,9 +139,23 @@
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
+// import {useFiles} from '~/hooks/api'
+// import {DocType} from '~/api/api'
 
 export default defineComponent({
   //  name: 'GenerateDocuments',
-  setup() {},
+  setup() {
+    // const selectPDF:DocType | null = null;
+    // const {createFile} = useFiles();
+
+    // const generateDocument = async () => {
+    //   if (selectPDF !== null) {await createFile({}, {type: 'DOCUMENT'}, {docType: selectPDF});}
+    // }
+
+    const selectPDF = 'none'
+    return {
+      selectPDF,
+    }
+  },
 })
 </script>
