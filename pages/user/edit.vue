9eb77c522f4725a3c1eee0ae682adde95887fc1e
Carter Grimmeisen <cpg0009@uah.edu>
2021-04-21 16:03:56 -0500

    Formatting updates on account pages

--- pages/user/edit.vue
+++ pages/user/edit.vue
@@ -1,75 +1,90 @@
 <!--EditAccount.vue-->
 <template>
-  <v-card class="mx-auto my-12" max-width="1000" width="1000">
-    <v-card-title>Edit Account</v-card-title>
-    <p>{{ success }}</p>
-    <v-card-text>
+  <v-container>
+    <v-card class="mx-auto">
+      <v-card-title class="text--primary">Edit Account</v-card-title>
+
+      <v-expand-transition>
+        <v-alert v-if="error">{{ error }}</v-alert>
+      </v-expand-transition>
+
       <v-card-title>Name:</v-card-title>
-      <v-text-field
-        v-if="$auth.user"
-        id="name"
-        v-model="name"
-        color="black"
-        hide-details
-        v.length="25"
-        single-line
-        outlined
-        :placeholder="$auth.user.name"
-        background-color="tertiary"
-        class="shrink mb-4"
-        append-icon="mdi-pen"
-      />
+      <v-card-text>
+        <v-text-field
+          v-if="$auth.user"
+          id="name"
+          v-model="name"
+          color="black"
+          hide-details
+          v.length="25"
+          single-line
+          outlined
+          :placeholder="$auth.user.name"
+          background-color="tertiary"
+          class="shrink mb-4"
+          append-icon="mdi-pen"
+        />
+      </v-card-text>
 
       <v-card-title>Email:</v-card-title>
-      <v-text-field
-        v-if="$auth.user"
-        v-model="email"
-        color="black"
-        hide-details
-        v.length="25"
-        single-line
-        outlined
-        :placeholder="$auth.user.email"
-        background-color="tertiary"
-        class="shrink mb-4"
-        append-icon="mdi-pen"
-      />
+      <v-card-text>
+        <v-text-field
+          v-if="$auth.user"
+          v-model="email"
+          color="black"
+          hide-details
+          v.length="25"
+          single-line
+          outlined
+          :placeholder="$auth.user.email"
+          background-color="tertiary"
+          class="shrink mb-4"
+          append-icon="mdi-pen"
+        />
+      </v-card-text>
 
       <v-card-title>New Password:</v-card-title>
 
-      <v-text-field
-        v-model="password"
-        color="black"
-        hide-details
-        v.length="25"
-        single-line
-        outlined
-        type="password"
-        placeholder="new password"
-        background-color="tertiary"
-        class="shrink"
-        append-icon="mdi-pen"
-      ></v-text-field>
-    </v-card-text>
+      <v-card-text>
+        <v-text-field
+          v-model="password"
+          color="black"
+          hide-details
+          v.length="25"
+          single-line
+          outlined
+          type="password"
+          placeholder="new password"
+          background-color="tertiary"
+          class="shrink"
+          append-icon="mdi-pen"
+        />
+      </v-card-text>
 
-    <v-divider></v-divider>
+      <v-divider></v-divider>
 
-    <v-card-actions>
-      <v-spacer />
-      <v-btn class="ma-2" color="primary" dark to="/user">
-        Cancel
-        <v-icon dark right>mdi-close</v-icon>
-      </v-btn>
-      <v-btn class="ma-2" color="primary" dark @click="update()">
-        Save
-        <v-icon dark right>mdi-content-save</v-icon>
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn class="ma-2" color="primary" dark to="/user" exact>
+          Cancel
+          <v-icon dark right>mdi-close</v-icon>
+        </v-btn>
+        <v-btn class="ma-2" color="primary" dark @click="update()">
+          Save
+          <v-icon dark right>mdi-content-save</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
 </template>
 
 <script lang="ts">
-import { defineComponent, useContext, ref } from '@nuxtjs/composition-api'
+import {
+  defineComponent,
+  useContext,
+  ref,
+  useRouter,
+} from '@nuxtjs/composition-api'
 import { useUser } from '~/hooks/api'
 
 export default defineComponent({
@@ -77,11 +92,12 @@ export default defineComponent({
   setup() {
     const { $auth } = useContext()
     const { updateUser } = useUser()
+    const $router = useRouter()
 
     const name = ref($auth.value.user!.name)
     const email = ref($auth.value.user!.email)
     const password = ref('')
-    const success = ref('')
+    const error = ref('')
 
     const update = async () => {
       const updator: any = {}
@@ -93,20 +109,19 @@ export default defineComponent({
         { id: $auth.value.user?.id },
         updator
       ).catch((e) => {
-        success.value = e.message
+        error.value = e.message
         return null
       })
 
-      if (user !== null) {
-        success.value = 'Success!'
-        $auth.value.user!.name = name.value
-        $auth.value.user!.email = email.value
-      }
+      if (user === null) return
 
-      return user
+      $auth.value.user!.name = name.value
+      $auth.value.user!.email = email.value
+
+      $router.replace('./')
     }
 
-    return { $auth, update, name, email, password, success }
+    return { $auth, update, name, email, password, error }
   },
 })
 </script>
--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,50 +1,40 @@
 <!--AccountPreview.vue -->
 <template>
-  <v-card
-    v-if="$auth.user"
-    class="mx-auto my-12"
-    max-width="1000"
-    width="1000"
-    height="400"
-  >
-    <v-card-title>Name:</v-card-title>
-    <div class="black--text ml-4" size="14" align="left">
-      {{ $auth.user.name }}
-    </div>
-    <v-card-title>Email:</v-card-title>
-    <div class="black--text ml-4" size="14" align="left">
-      {{ $auth.user.email }}
-    </div>
-    <div v-if="$auth.user.agentProfile">
-      <v-card-title>Phone:</v-card-title>
-      <div class="black--text ml-4" size="14" align="left">
-        {{ $auth.user.agentProfile.phone }}
-      </div>
-      <v-card-title>Agency Name:</v-card-title>
-      <div class="black--text ml-4" size="14" align="left">
-        {{ $auth.user.agentProfile.agency.name }}
-      </div>
-    </div>
+  <v-container>
+    <v-card v-if="$auth.user" class="mx-auto">
+      <v-card-title>Name:</v-card-title>
+      <v-card-text>
+        <div class="black--text ml-4" size="14" align="left">
+          {{ $auth.user.name }}
+        </div>
+      </v-card-text>
+      <v-card-title>Email:</v-card-title>
+      <v-card-text>
+        <div class="black--text ml-4" size="14" align="left">
+          {{ $auth.user.email }}
+        </div>
+      </v-card-text>
+      <template v-if="$auth.user.agentProfile">
+        <v-card-title>Phone:</v-card-title>
+        <v-card-text class="black--text ml-4" size="14" align="left">
+          {{ $auth.user.agentProfile.phone }}
+        </v-card-text>
+        <v-card-title>Agency Name:</v-card-title>
+        <v-card-text class="black--text ml-4" size="14" align="left">
+          {{ $auth.user.agentProfile.agency.name }}
+        </v-card-text>
+      </template>
 
-    <!--
-    <v-card-text>
-      <v-row class="mx-0"> </v-row>
-      <v-row>
-        <div class="black--text ml-4" size="14">$###,###</div>
-        <div class="black--text ml-4" size="14">Sqft: #,###</div>
-        <div class="black--text ml-4" size="14">MLS:#######</div>
-      </v-row>
-    </v-card-text>
--->
-    <v-divider class="mx-4"></v-divider>
+      <v-divider class="mx-4"></v-divider>
 
-    <v-card-actions>
-      <v-btn class="ma-2" color="primary" dark to="/user/edit">
-        Edit Details
-        <v-icon dark right> mdi-cog-outline </v-icon>
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+      <v-card-actions>
+        <v-btn class="ma-2" color="primary" dark to="/user/edit">
+          Edit Details
+          <v-icon dark right> mdi-cog-outline </v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
 </template>
 
 <script lang="ts">
